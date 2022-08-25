fcc790c3b88ebb497dbe9631c51ab48360083cd4
Mykola Hlushchenko <mykola.hlushchenko@onix-systems.com>
2022-08-25 21:51:51 +0300

    Update all__helpers.js

--- all__helpers.js
+++ all__helpers.js
@@ -2,10 +2,11 @@
 // file:///F:/Projects/0Tampermonkey0/jquery-3.4.1-jQueryMod-noconflict.js
 // https://raw.githubusercontent.com/Untiy16/links/master/jquery-3.4.1-jQueryMod-noconflict.js
 // https://raw.githubusercontent.com/Untiy16/links/master/copyLinkText6.js?q=3
-
+//https://cdn.jsdelivr.net/gh/Untiy16/links@master/jquery-3.4.1-jQueryMod-noconflict.js
+//https://untiy16.s-host.net/api/tampermonkey-scripts/jquery-3.4.1-jQueryMod-noconflict.js
 
 let jQueryModScript= document.createElement('script');
-jQueryModScript.setAttribute('src','https://cdn.jsdelivr.net/gh/Untiy16/links@master/jquery-3.4.1-jQueryMod-noconflict.js');
+jQueryModScript.setAttribute('src','https://untiy16.s-host.net/api/tampermonkey-scripts/jquery-3.4.1-jQueryMod-noconflict.js');
 document.head.appendChild(jQueryModScript);
 
 var code = `window.c = console.log.bind(console); window.GM_addStyle = function(a){let b=document.getElementById("GM_addStyleBy8626")||function(){let a=document.createElement("style");return a.type="text/css",a.id="GM_addStyleBy8626",document.head.appendChild(a),a}();b.appendChild(document.createTextNode(a))}; window.untiy16Url = 'https://untiy16.s-host.net/api'; window.untiy16JikanHost = 'https://api.jikan.moe';`;
